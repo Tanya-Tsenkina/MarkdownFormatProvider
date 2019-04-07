3b42b455b06bd6c5eee307f7c8728ce0d6a38da8
Tanya-Tsenkina <tanya.tsenkina@gmail.com>
2019-04-07 23:45:52 +0300

    Add files via upload

---
+++ MarkdownFormatProvider/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Windows;
+using Telerik.Windows.Controls;
+using System.Windows.Forms;
+using System;
+
+namespace MarkdownFormatProvider_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string outPath;
+
+        public MainWindow()
+        {
+            //RadRichTextBox.DefaultTextRenderingMode = Telerik.Windows.Documents.UI.TextBlocks.TextBlockRenderingMode.TextBlockWithPropertyCaching;
+            InitializeComponent();
+
+            var provider = new MarkDownFormatProvider();
+            this.radRichTextBox.Document = provider.Import(new FileStream("../../SampleData/README.md", FileMode.Open));
+        }
+
+        private void ToPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                outPath = dialog.SelectedPath;
+                this.toPathTextBox.Text = outPath;
+            }
+        }
+
+        private void FromPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            radRichTextBox.Commands.OpenDocumentCommand.Execute();
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.toPathTextBox.Text))
+            {
+                System.Windows.MessageBox.Show("To path is not specified.");
+                return;
+            }
+            var provider = new MarkDownFormatProvider();
+
+            try
+            {
+                FileStream writer = new FileStream(outPath + "\\hide.html", FileMode.Create);
+                using (writer)
+                {
+                    provider.OutPath = outPath;
+                    provider.Export(radRichTextBox.Document, writer);
+                }
+                File.Delete(outPath + "\\hide.html");
+                System.Windows.MessageBox.Show("Export completed successfully.");
+            }
+            catch (IOException ex)
+            {
+                System.Windows.MessageBox.Show(LocalizationManager.GetString("Documents_OpenDocumentCommand_TheFileIsLocked"));
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(LocalizationManager.GetString("Documents_OpenDocumentCommand_TheFileCannotBeOpened"));
+            }
+
+        }
+    }
+}
+
---
+++ MarkdownFormatProvider/MarkDownFormatProvider.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Telerik.Windows.Documents.FormatProviders;
+using Telerik.Windows.Documents.FormatProviders.Html;
+using Telerik.Windows.Documents.Model;
+using CommonMark;
+using System;
+using Aspose.Html.Saving;
+using Aspose.Html;
+
+namespace MarkdownFormatProvider_WPF
+{
+    [CustomDocumentFormatProvider]
+    class MarkDownFormatProvider : DocumentFormatProviderBase
+    {
+        private static readonly string name = "MarkDownFormatProvider";
+        private static readonly IEnumerable<string> supportedExtensions = new string[] { ".md", ".markdown" };
+        private readonly HtmlFormatProvider htmlProvider;
+        private static string outPath;
+
+        public MarkDownFormatProvider()
+        {
+            this.htmlProvider = new HtmlFormatProvider();
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return MarkDownFormatProvider.name;
+            }
+        }
+
+        public override string FilesDescription
+        {
+            get
+            {
+                return "MarkDown Document";
+            }
+        }
+
+        public string OutPath
+        {
+            get
+            {
+                return outPath;
+            }
+            set
+            {
+                outPath = value;
+            }
+        }
+
+        public override IEnumerable<string> SupportedExtensions
+        {
+            get
+            {
+                return MarkDownFormatProvider.supportedExtensions;
+            }
+        }
+        public override bool CanImport
+        {
+            get { return true; }
+        }
+
+        public override bool CanExport
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override RadDocument Import(Stream input)
+        {
+            using (StreamReader reader = new StreamReader(input))
+            {
+                // Performs the first stage of the conversion - parses block elements from the source
+                // and created the syntax tree.
+                var blockDoc = CommonMarkConverter.ProcessStage1(reader);
+
+                //Performs the second stage of the conversion - parses block element contents into
+                //inline elements.
+                CommonMarkConverter.ProcessStage2(blockDoc);
+
+                string tempFileName = System.IO.Path.GetTempFileName();
+                StreamWriter writer = new StreamWriter(tempFileName);
+                using (writer)
+                {
+                    // Performs the last stage of the conversion - converts the syntax tree to HTML
+                    // representation.
+                    CommonMarkConverter.ProcessStage3(blockDoc, writer);
+                }
+
+                RadDocument document;
+                using (FileStream stream = File.OpenRead(tempFileName))
+                {
+                    document = htmlProvider.Import(stream);
+                }
+                File.Delete(tempFileName);
+
+                return document;
+            }
+        }
+
+        public override void Export(RadDocument document, Stream output)
+        {
+
+            //export as HTML
+            using (output)
+            {
+                htmlProvider.Export(document, output);
+            }
+            //using Aspose.HTML to convert html to markdown
+            var documentap = new HTMLDocument(outPath + "\\hide.html");
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            documentap.Save(Path.GetFullPath(outPath + "\\MarkDownFile.md"), HTMLSaveFormat.Markdown);
+
+        }
+    }
+}
+
